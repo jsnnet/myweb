b8c137946282d8bb60915d2421436c8b97d31ad2
jsnnet <ods8703@gmail.com>
2020-02-18 11:11:47 +0900

    final0217_7.36pm

--- finalProject/urls.py
+++ finalProject/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout', views.logout),
     path('qna1',views.qna1),
     path('myquestion',views.myquestion),
+    path('myquestion2',views.myquestion2),
     path('myQlist2',views.myQlist2),
     path('notice1',views.notice1),
     path('ride1',views.ride1),
